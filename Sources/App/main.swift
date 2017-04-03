ca4bc00728662a440825371a31d30c75e74a656b
ohyes1h <u9635001@gmail.com>
2017-04-03 16:56:59 +0800

    Add acronym model

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,29 @@
 import Vapor
+import VaporPostgreSQL
 
-let drop = Droplet()
+let drop = Droplet(
+    preparations: [Acronym.self],
+    providers: [VaporPostgreSQL.Provider.self]
+)
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get("version") { request in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    } else {
+        return "No db connection"
+    }
 }
 
-drop.resource("posts", PostController())
+drop.get("model") { request in
+    let acronym = Acronym(short: "AFK", long: "Away From Keyboard")
+    return try acronym.makeJSON()
+}
+
+drop.get("test") { request in
+    var acronym = Acronym(short: "AFK", long: "Away From Keyboard")
+    try acronym.save()
+    return try JSON(node: Acronym.all().makeNode())
+}
 
 drop.run()
